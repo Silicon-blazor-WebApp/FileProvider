eae067fb14e4a75a350da60f598ba2c5a1847653
hugospangberg <hugospangberg@HUGOSPNGBER517B>
2024-05-30 12:28:38 +0200

    Add project files.

---
+++ Data/Contexts/DataContextFactory.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Contexts
+{
+    public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+    {
+        public DataContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+            optionsBuilder.UseSqlServer("Server=tcp:sqlserver-silicon-blazor.database.windows.net,1433;Initial Catalog=SqlSiliconDatabaseMain;Persist Security Info=False;User ID=sqlAdmin;Password=Bytmig123!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+
+            return new DataContext(optionsBuilder.Options);
+        }
+    }
+}
